61cca381bdec8ab16e9865da0eabfc462130692c
Steven L. Speek <slspeek@gmail.com>
2015-11-25 23:56:58 +0100

    hpc coverage script

---
+++ cover.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+cabal test
+mkdir -p dist/build/hpc
+hpc markup --exclude="Main" --exclude="HEphem.TestUtils" --exclude="HEphem.HEphemSpec" --exclude="HEphem.UISpec" --exclude="HEphem.BSParserSpec" spec
+mv *.html dist/build/hpc
